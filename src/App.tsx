61f31f25101b65863c35a6b9d55f377fc24dec5a
Stanislav Akhmadeev <stanislav.akhmadeev@idp.zyfra.com>
2023-04-26 11:48:04 +0300

    feat: add lazy-components and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, { Suspense } from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
 import "./index.scss";
+import { AboutPageLazy } from "./pages/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage.lazy";
 
 const App = () => {
     return (
-        <div>
-            <Counter />
+        <div className="app">
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>Об нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageLazy />} />
+                    <Route path='/' element={<MainPageLazy />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
